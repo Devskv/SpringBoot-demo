8610383d51a1d5231ae09505aa9a5ef9037a09e8
DirVanga <dirvanga@gmail.com>
2018-06-21 11:42:59 -0500

    added docker file

--- src/main/java/com/stationfinder/demo/domain/StationRepository.java
+++ src/main/java/com/stationfinder/demo/domain/StationRepository.java
@@ -1,6 +1,7 @@
 package com.stationfinder.demo.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
