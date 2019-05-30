656c8f715475507323cc8b845b59f6d1888fb262
DirVanga <dirvanga@gmail.com>
2019-05-30 16:05:59 -0500

    adding eureka registry config

--- src/main/java/com/stationfinder/demo/DemoApplication.java
+++ src/main/java/com/stationfinder/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package com.stationfinder.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.stationfinder.demo.domain")
 @SpringBootApplication
 public class DemoApplication {
