02d191e31639052b65f01830a0727fc02a411537
DirVanga <dirvanga@gmail.com>
2019-08-12 00:02:41 -0400

    war packaging changes

--- src/main/java/com/stationfinder/demo/DemoApplication.java
+++ src/main/java/com/stationfinder/demo/DemoApplication.java
@@ -2,15 +2,22 @@ package com.stationfinder.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.stationfinder.demo.domain")
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
 }
